9c7cf113e84738cccadb0bc2a08f9e56d05dc2cb
Joseph Kao <josephtkao@gmail.com>
2020-06-16 20:42:53 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -58,6 +58,13 @@ class Dog
   end
 
   def self.find_or_create_by(name, breed)
-
+    sql = <<-SQL
+    SELECT * FROM dogs WHERE name = ? AND breed = ?
+    SQL
+    dog = DB[:conn].execute(sql, name, breed)
+    if dog.empty?
+      song = self.create(:name, :breed)
+    else
+      
 
 end
